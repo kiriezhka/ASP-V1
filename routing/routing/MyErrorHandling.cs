486480bebced10224c44f07b3f89adcddd2ee7d1
kiriezhka <92943800+kiriezhka@users.noreply.github.com>
2022-12-02 14:22:38 +0100

    v 2.0
    v 2.0

---
+++ routing/routing/MyErrorHandling.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace routing
+{
+    public class MyErrorHandling
+    {
+        private readonly RequestDelegate _next;
+
+        public MyErrorHandling(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = 404;
+                
+                
+            }
+        }
+    }
+}
